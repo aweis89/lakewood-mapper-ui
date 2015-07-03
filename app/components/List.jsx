f080a5c62f74fbda9c802bfeae89e6cdaab52d4d
Aaron Weisberg <Aaron.Weisberg@lendkey.com>
2015-07-03 16:03:10 -0400

    add working location markers

--- app/components/List.jsx
+++ app/components/List.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
-class List extends React.Component {
+export default class List extends React.Component {
+
+  componentDidMount () {
+    this.props.items.map(function(item) {
+      window.Marker.add(item);
+    });
+  }
+
+  componentDidUpdate () {
+    this.props.items.map(function(item) {
+      window.Marker.add(item);
+    });
+  }
+
   render () {
     return (
       <ul>
         {
           this.props.items.map(function(item) {
-           return <li key={item.id}>{item.name} : {item.id}</li>;
+           return <li key={item.id}>{item.name}</li>;
           })
         }
       </ul>
@@ -14,4 +27,3 @@ class List extends React.Component {
   }
 }
 
-export default List;
--- app/components/mapComponent.js
+++ app/components/mapComponent.js
@@ -24,13 +24,42 @@ $(document).ready(function() {
 
     view: new ol.View({
       center: ol.proj.transform(
-        [-74.2218196, 40.0957316], 
-        'EPSG:4326', 
+        [-74.2218196, 40.0957316],
+        'EPSG:4326',
         'EPSG:3857'
       ),
-      zoom: 15,
-      minZoom: 12
+      zoom: 12,
+      minZoom: 10
     })
 
   });
 });
+
+window.Marker = {
+  add: function (address) {
+    var lon = address.longitude;
+    var lat = address.latitude;
+    var feature = new ol.Feature({
+      name: address.name,
+      geometry: new ol.geom.Point(ol.proj.transform([Number(lon), Number(lat)], 'EPSG:4326', 'EPSG:3857'))
+    });
+
+    var vectorSource = new ol.source.Vector({
+      features: [feature]
+    });
+    var iconStyle = new ol.style.Style({
+      image: new ol.style.Icon({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        opacity: 0.75,
+        src: 'http://ol3js.org/en/master/examples/data/icon.png'})
+    });
+    var vectorLayer = new ol.layer.Vector({
+      source: vectorSource,
+      style: iconStyle
+    });
+    window.map.addLayer(vectorLayer);
+  }
+};
+
