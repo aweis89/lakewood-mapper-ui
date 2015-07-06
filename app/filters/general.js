c8a6e9fd4a737e9737c4dfe9b8b78cf318da2300
Aaron Weisberg <Aaron.Weisberg@lendkey.com>
2015-07-06 11:32:35 -0400

    comments out distance function as token minification is erroring

--- app/filters/general.js
+++ app/filters/general.js
@@ -11,28 +11,28 @@ var Filters = {
       return match !== -1;
     });
     return filtered;
-  },
-
-  distance: function (longLat, longLat2) {
-    var lat1 = longLat.latitude;
-    var lon1 = longLat.longitude;
-
-    var lon2 = longLat2.longitude;
-    var lat2 = longLat2.latitude;
-
-    var R = 6371000; // metres
-    var φ1 = lat1.toRadians();
-    var φ2 = lat2.toRadians();
-    var Δφ = (lat2 - lat1).toRadians();
-    var Δλ = (lon2 - lon1).toRadians();
-
-    var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-      Math.cos(φ1) * Math.cos(φ2) *
-      Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    return R * c;
   }
+
+  //distance: function (longLat, longLat2) {
+    //var lat1 = longLat.latitude;
+    //var lon1 = longLat.longitude;
+
+    //var lon2 = longLat2.longitude;
+    //var lat2 = longLat2.latitude;
+
+    //var R = 6371000; // metres
+    //var φ1 = lat1.toRadians();
+    //var φ2 = lat2.toRadians();
+    //var Δφ = (lat2 - lat1).toRadians();
+    //var Δλ = (lon2 - lon1).toRadians();
+
+    //var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+      //Math.cos(φ1) * Math.cos(φ2) *
+      //Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    //var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    //return R * c;
+  //}
 };
 
 export default Filters;
